c6aeb6c0d70bcfde147d5d6ea31fb4e824e3ba36
DecorteKasper <91284526+DecorteKasper@users.noreply.github.com>
2022-10-06 14:59:46 +0200

    toggle password

--- script/app.js
+++ script/app.js
@@ -3,8 +3,19 @@ function handleFloatingLabel() {
 }
 
 function handlePasswordSwitcher() {
-    
-}
+    const togglePasswordInput = document.querySelector('.c-toggle-password__checkbox'
+    );
+
+    togglePasswordInput.addEventListener('change', function () { 
+        const passwordInput = document.querySelector('.js-password');
+        
+        if (this.checked) {
+            passwordInput.type = 'text';
+        } else {
+            passwordInput.type = 'password';
+        }
+    });
+};
 
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Script loaded!');
